fc6faff3557403b5d0fd1a0583b61f06ca97439f
Sudipto Das <shiningsudipto@gmail.com>
2023-06-19 00:38:15 +0600

    home fixed

--- src/Pages/Home/GridSection.jsx
+++ src/Pages/Home/GridSection.jsx
@@ -1,18 +1,18 @@
 
 const GridSection = () => {
     return (
-        <div>
-            <div className="row g-5">
-                <div className="col grass">
-                    <div className="p-5">
+        <div className="my-5">
+            <div className="row g-4">
+                <div className="col-3 ">
+                    <div className="p-5 grass">
                         <h3>MY ADS</h3>
                         <div></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur blanditiis facere, suscipit atque doloribus ab?</p>
                         <button className="viewMore">view more</button>
                     </div>
                 </div>
-                <div className="col camel">
-                    <div className="p-5">
+                <div className="col-5 ">
+                    <div className="p-5 camel">
                         <h3>$50.99</h3>
                         <div>
                             <h2>Camel</h2>
@@ -21,8 +21,8 @@ const GridSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col goat">
-                    <div className="p-5">
+                <div className="col-4 ">
+                    <div className="p-5 goat">
                         <h3>$50.99</h3>
                         <div>
                             <h2>Camel</h2>
@@ -32,9 +32,9 @@ const GridSection = () => {
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col sheep">
-                    <div className="p-5">
+            <div className="row my-4">
+                <div className="col-4">
+                    <div className="p-5 sheep">
                         <h3>$30.99</h3>
                         <div>
                             <h2>SHEEP</h2>
@@ -43,8 +43,8 @@ const GridSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col cow">
-                    <div className="p-5">
+                <div className="col-8">
+                    <div className="p-5 cow">
                         <h3>$45.99</h3>
                         <div>
                             <h2>COW</h2>
